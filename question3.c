cd0def73cacfb20683f2b8ee3403d510d73756c2
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-29 18:29:23 +0530

    Update question3.c

---
+++ question3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Program to display the size of different data types
+// kartikSingh
+// 2303511530009
+
+int main() {
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of char: %zu bytes\n", sizeof(char));
+
+    return 0;
+}
