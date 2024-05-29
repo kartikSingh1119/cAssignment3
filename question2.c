c59ec6c5e69dbf7ee86a142a5453f59ba690ba44
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-29 18:28:51 +0530

    Update question2.c

---
+++ question2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Program to implement a simple calculator
+// kartikSingh
+// 2303511530009
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error! Division by zero.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Error! Operator is not correct.\n");
+            return 1;
+    }
+
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
